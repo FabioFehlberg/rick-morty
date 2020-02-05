de3c339459b8b692c632bb26492c6aca8037190b
fehlberg <fabio.chaves.f@gmail.com>
2020-02-05 16:09:40 -0300

    Include UI tests for main activity

---
+++ app/src/androidTest/java/com/fehlves/rickmorty/MainActivityRobot.kt
@@ -0,0 +1,43 @@
+package com.fehlves.rickmorty
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.fehlves.rickmorty.main.MainActivity
+
+
+class MainActivityRobot {
+
+    private lateinit var scenario: ActivityScenario<MainActivity>
+
+    fun launchActivity() = apply {
+        scenario = ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    //region: given
+
+
+    //endregion
+
+    //region: when
+
+    fun whenTitleAppear() = apply {
+        onView(withId(R.id.ivLogo)).check(matches(isDisplayed()))
+    }
+
+    fun whenListHasAllItems(count: Int) = apply {
+        onView(withId(R.id.rvMain)).check(matches(hasChildCount(count)))
+    }
+
+    //endregion
+
+    //region: then
+
+    fun thenTitleHasDescription() = apply {
+        onView(withId(R.id.ivLogo)).check(matches(hasContentDescription()))
+    }
+
+    //endregion
+
+}
