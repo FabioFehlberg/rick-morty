7897091d608b34b112b9e00604a4fab1ac6dafec
fehlberg <fabio.chaves.f@gmail.com>
2020-02-06 16:15:29 -0300

    Add episode layout and model for catalogue

---
+++ app/src/main/java/com/fehlves/rickmorty/catalogue/model/EpisodeCardView.kt
@@ -0,0 +1,10 @@
+package com.fehlves.rickmorty.catalogue.model
+
+import com.fehlves.rickmorty.common.BaseView
+
+data class EpisodeCardView(
+    val name: String,
+    val episode: String,
+    val airDate: String,
+    val onClick: (String) -> Unit
+) : BaseView()
