cb91bce0912eb081a85cc716c8bbddf81ea8a0a1
fehlberg <fabio.chaves.f@gmail.com>
2020-02-06 16:44:00 -0300

    Set content description for search icon inside xml

--- app/src/main/java/com/fehlves/rickmorty/catalogue/model/SearchView.kt
+++ app/src/main/java/com/fehlves/rickmorty/catalogue/model/SearchView.kt
@@ -3,6 +3,5 @@ package com.fehlves.rickmorty.catalogue.model
 import com.fehlves.rickmorty.common.BaseView
 
 data class SearchView(
-    val searchIconContentDescription: Int,
     val onSearchClick: () -> Unit
 ) : BaseView()
