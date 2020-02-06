ed72d4389a6303ed33804e8b083120a5e306cb15
fehlberg <fabio.chaves.f@gmail.com>
2020-02-06 15:58:04 -0300

    Add character layout and model for catalogue

---
+++ app/src/main/java/com/fehlves/rickmorty/catalogue/model/CharacterCardView.kt
@@ -0,0 +1,11 @@
+package com.fehlves.rickmorty.catalogue.model
+
+import com.fehlves.rickmorty.common.BaseView
+
+data class CharacterCardView(
+    val name: String,
+    val specie: String,
+    val gender: String,
+    val status: String,
+    val onClick: (String) -> Unit
+) : BaseView()
