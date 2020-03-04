541b658197340d1b364370dd5b945816c2696d29
fehlberg <fabio.chaves.f@gmail.com>
2020-03-04 17:45:01 -0300

    Refactor

--- app/src/main/java/com/fehlves/rickmorty/detail/CharacterDetailActivity.kt
+++ app/src/main/java/com/fehlves/rickmorty/detail/CharacterDetailActivity.kt
@@ -5,15 +5,22 @@ import android.content.Intent
 import android.os.Bundle
 import com.fehlves.rickmorty.R
 import com.fehlves.rickmorty.common.BaseActivity
+import com.fehlves.rickmorty.common.Constants.Companion.CHARACTER_TYPE
+import com.fehlves.rickmorty.extensions.extra
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class CharacterDetailActivity : BaseActivity() {
 
-    //private val viewModel: CatalogueViewModel by viewModel()
+    private val viewModel: DetailViewModel by viewModel()
+
+    private val characterId by extra<Int>(ARG_CHARACTER_ID)
 
     override fun getContentLayoutId() = R.layout.activity_catalogue
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        viewModel.loadDetails(characterId, CHARACTER_TYPE)
 
         setupObservables()
     }
@@ -23,11 +30,11 @@ class CharacterDetailActivity : BaseActivity() {
     }
 
     companion object {
-        private const val ARG_SELECTED_TYPE = "arg_selected_type"
+        private const val ARG_CHARACTER_ID = "arg_character_id"
 
-        fun newInstance(context: Context, selectedType: Int) =
+        fun newInstance(context: Context, characterId: Int) =
             Intent(context, CharacterDetailActivity::class.java).apply {
-                putExtra(ARG_SELECTED_TYPE, selectedType)
+                putExtra(ARG_CHARACTER_ID, characterId)
             }
     }
 }
