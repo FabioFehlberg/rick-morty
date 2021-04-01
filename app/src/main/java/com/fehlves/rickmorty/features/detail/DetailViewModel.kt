9a2d50223cd9ae401bc8cd69cf01fc329e04a9ba
Fabio Fehlberg <fabiochavesf@gmail.com>
2021-04-01 17:52:16 -0300

    Remove location for detail

--- app/src/main/java/com/fehlves/rickmorty/features/detail/DetailViewModel.kt
+++ app/src/main/java/com/fehlves/rickmorty/features/detail/DetailViewModel.kt
@@ -6,7 +6,6 @@ import com.fehlves.rickmorty.common.BaseResult
 import com.fehlves.rickmorty.common.BaseViewModel
 import com.fehlves.rickmorty.data.CharacterEntity
 import com.fehlves.rickmorty.data.EpisodeEntity
-import com.fehlves.rickmorty.data.LocationEntity
 import com.fehlves.rickmorty.data.detail.DetailRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -15,11 +14,9 @@ import kotlinx.coroutines.withContext
 class DetailViewModel(private val detailRepository: DetailRepository) : BaseViewModel() {
 
     private val onCharacterListResult = MutableLiveData<List<CharacterEntity>?>()
-    private val onLocationListResult = MutableLiveData<List<LocationEntity>?>()
     private val onEpisodeListResult = MutableLiveData<List<EpisodeEntity>?>()
 
     fun onCharacterListResult(): LiveData<List<CharacterEntity>?> = onCharacterListResult
-    fun onLocationListResult(): LiveData<List<LocationEntity>?> = onLocationListResult
     fun onEpisodeListResult(): LiveData<List<EpisodeEntity>?> = onEpisodeListResult
 
     fun loadCharacterList(ids: String) {
@@ -27,15 +24,6 @@ class DetailViewModel(private val detailRepository: DetailRepository) : BaseView
             when (val result = withContext(Dispatchers.IO) { detailRepository.getCharacterList(ids) }) {
                 is BaseResult.Success -> onCharacterListResult.postValue(result.data)
                 is BaseResult.Error -> onCharacterListResult.postValue(null)
-            }
-        }
-    }
-
-    fun loadLocationsList(ids: String) {
-        launch {
-            when (val result = withContext(Dispatchers.IO) { detailRepository.getLocationList(ids) }) {
-                is BaseResult.Success -> onLocationListResult.postValue(result.data)
-                is BaseResult.Error -> onLocationListResult.postValue(null)
             }
         }
     }
@@ -54,15 +42,6 @@ class DetailViewModel(private val detailRepository: DetailRepository) : BaseView
             when (val result = withContext(Dispatchers.IO) { detailRepository.getCharacter(id) }) {
                 is BaseResult.Success -> onCharacterListResult.postValue(listOf(result.data))
                 is BaseResult.Error -> onCharacterListResult.postValue(null)
-            }
-        }
-    }
-
-    fun loadLocation(id: Int) {
-        launch {
-            when (val result = withContext(Dispatchers.IO) { detailRepository.getLocation(id) }) {
-                is BaseResult.Success -> onLocationListResult.postValue(listOf(result.data))
-                is BaseResult.Error -> onLocationListResult.postValue(null)
             }
         }
     }
