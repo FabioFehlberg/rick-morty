ae995f3973432ff287bb71f6010fe6dcfd17f353
FabioFehlberg <fabio.chaves.f@gmail.com>
2022-01-10 22:39:37 -0300

    Update dependencies

--- app/src/main/java/com/fehlves/rickmorty/common/RickMortyApp.kt
+++ app/src/main/java/com/fehlves/rickmorty/common/RickMortyApp.kt
@@ -1,6 +1,7 @@
 package com.fehlves.rickmorty.common
 
 import android.app.Application
+import com.fehlves.rickmorty.BuildConfig
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -10,7 +11,7 @@ class RickMortyApp : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidLogger(Level.DEBUG)
+            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
             androidContext(this@RickMortyApp)
             modules(mutableListOf(repositoryModule, retrofitModule, apiModule, detailRepositoryModule, detailApiModule).apply {
                 addAll(
